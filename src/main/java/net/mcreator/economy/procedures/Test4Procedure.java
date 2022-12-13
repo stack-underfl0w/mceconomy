17e505a2ee8bcd9f193c39df2982ed376b03a327
stack-underfl0w <us.nahmad@gmail.com>
2022-12-13 17:32:39 +0000

    Base New Version

--- src/main/java/net/mcreator/economy/procedures/Test4Procedure.java
+++
@@ -1,6 +0,0 @@
-package net.mcreator.economy.procedures;
-
-public class Test4Procedure {
-	public static void execute() {
-	}
-}
--- src/main/java/net/mcreator/economy/procedures/TestProcedure.java
+++
@@ -1,6 +0,0 @@
-package net.mcreator.economy.procedures;
-
-public class TestProcedure {
-	public static void execute() {
-	}
-}
